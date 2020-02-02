90e1a21e0455003e9d317af1b72d576ddee25c81
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2020-02-01 22:14:06 -0600

    V2.2
    database addition

--- register.php
+++ register.php
@@ -21,9 +21,9 @@
             <label><b>Password:</b></label><br>
             <input name = "password" type="password" class="inputvalues" placeholder="Type your password" required/><br> 
             <label><b>Confirm Password:</b></label><br>
-            <input name = "cpassword" type="password" class="inputvalues" placeholder="Confirm password"required/><br> 
+            <input name = "cpassword" type="password" class="inputvalues" placeholder="Confirm password" required/><br> 
             <input name = "submit_btn" type="submit" id="signup_btn" value="Sign Up"/><br>
-            <input type="button" id="back_btn" value="Back to login"/>
+            <a href = "index.php"><input type="button" id="back_btn" value="Back to login"/></a>
         </form>
         
         <?php
@@ -33,7 +33,39 @@
 
                 $username = $_POST['username'];
                 $password = $_POST['password'];
-                $cpassword = $_POST['password'];
+                $cpassword = $_POST['cpassword'];
+
+                if($password == $cpassword)
+                {
+                    $query = "select * FROM userinfo WHERE username = '$username'";   
+                    $query_run = mysqli_query($con,$query);
+
+                    if(mysqli_num_rows($query_run)>0)
+                    {
+                        //already a same username exits
+                        echo '<script type = "text/javascript"> alert("Username exists. Try another username") </script>';
+                    }
+                    else
+                    {
+                        $query = "insert INTO userinfo values('$username','$password')"; 
+                        $query_run = mysqli_query($con,$query);  
+
+                        if($query_run)
+                        {
+                            echo '<script type = "text/javascript"> alert("Congratulations! You are registered now.") </script>';
+                        }
+                        else
+                        {
+                            echo '<script type = "text/javascript"> alert("Error!") </script>';
+                        }
+
+                    }
+                }
+                else
+                {
+                    echo '<script type = "text/javascript"> alert("Error! Passwords did not match.") </script>';
+                }
+
             }
         ?>
         
