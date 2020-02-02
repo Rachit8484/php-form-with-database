5c258f16c64212dda54d997f815211fa67a14f9f
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2020-02-01 22:32:57 -0600

    V2.3
    homepage entry

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    require 'db/config.php';
 ?>
 
 <!DOCTYPE html>
@@ -15,16 +15,36 @@
             <img src = "img/avatar.png" class = "avatar"/>
         </center>
   
-    <form class="myform" action = "index.php" method = "post">
-    <label><b>Username:</b></label>
-    <input type="text" class="inputvalues" placeholder="Type your username"/><br>
-    <label><b>Password:</b></label>
-    <input type="password" class="inputvalues" placeholder="Type your password"/><br> 
-    <input type="submit" id="login_btn" value="Login"/><br>
-    <input type="button" id="register_btn" value="Resgister"/>
+        <form class="myform" action = "index.php" method = "post">
+            <label><b>Username:</b></label>
+            <input name = "username" type="text" class="inputvalues" placeholder="Type your username"/><br>
+            <label><b>Password:</b></label>
+            <input name = "password" type="password" class="inputvalues" placeholder="Type your password"/><br> 
+            <input name = "login" type="submit" id="login_btn" value="Login"/><br>
+            <a href ="register.php"><input type="button" id="register_btn" value="Register"/></a>
+        </form>
+        <?php
+        if(isset($_POST['login']))
+        {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $query = "select * from userinfo WHERE username = '$username' AND password = '$password'";
 
-    
-    </form>
+            $query_run = mysqli_query($con, $query);
+            if(mysqli_num_rows($query_run)>0)
+            {
+                //valid
+                $_SESSION['username'] = $username;
+                header('location:homepage.php');
+            }
+            else
+            {
+                //invalid
+                echo '<script type = "text/javascript"> alert("Error! invalid credentials") </script>';
+
+            }
+        }
+        ?>
     </div> 
 
 
